b631b47d86ff3e798f00c47d2630440411195b6e
Yadwinder Singh <ryker1990@gmail.com>
2016-11-04 13:53:58 +0530

    Update MCP3427.py

--- Onion Omega Python/MCP3427.py
+++ Onion Omega Python/MCP3427.py
@@ -24,7 +24,7 @@ data = i2c.readBytes(0x68, 0x00, 2)
 # Convert the data to 12-bits
 raw_adc = (data[0] & 0x0F) * 256 + data[1]
 if raw_adc > 2047 :
-	raw_adc -= 4096
+	raw_adc -= 4095
 
 # Output data to screen
 print "Digital Value of Analog Input : %d" %raw_adc
