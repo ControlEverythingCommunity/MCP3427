cac71db062d79f59e85977dd8a0f517a0e6dfb34
Yadwinder SIngh <apple@Yaddis-iMac.local>
2016-12-07 18:32:24 +0530

    2-Channel Code Update

---
+++ Java/MCP3427_2Channel.java
@@ -0,0 +1,61 @@
+// Distributed with a free-will license.
+// Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
+// MCP3427
+// This code is designed to work with the MCP3427_I2CADC I2C Mini Module available from ControlEverything.com.
+// https://www.controleverything.com/content/Analog-Digital-Converters?sku=MCP3427_I2CADC#tabs-0-product_tabset-2
+
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CDevice;
+import com.pi4j.io.i2c.I2CFactory;
+import java.io.IOException;
+
+public class MCP3427_2Channel
+{
+	public static void main(String args[]) throws Exception
+	{
+		// Create I2C bus
+		I2CBus Bus = I2CFactory.getInstance(I2CBus.BUS_1);
+		// Get I2C device, MCP3427 I2C address is 0x68(104)
+		I2CDevice device = Bus.getDevice(0x68);
+
+		// Send configuration command
+		// Continuous conversion mode, Channel-1, 12-bit Resolution
+		device.write((byte)0x10);
+		Thread.sleep(500);
+
+		// Read 2 bytes of data
+		// raw_adc msb, raw_adc lsb
+		byte[] data = new byte[2];
+		device.read(0x00, data, 0, 2);
+
+		// Convert the data to 12-bits
+		int raw_adc = ((data[0] & 0x0F)* 256 + (data[1] & 0xFF));
+		if(raw_adc > 2047)
+		{
+			raw_adc -= 4095;
+		}
+
+		// Output data to screen
+		System.out.printf("Digital value of Analog input on Channel-1: %d %n", raw_adc);
+
+		// Send configuration command
+		// Continuous conversion mode, Channel-2, 12-bit Resolution
+		device.write((byte)0x30);
+		Thread.sleep(500);
+
+		// Read 2 bytes of data
+		// raw_adc msb, raw_adc lsb
+		byte[] data1 = new byte[2];
+		device.read(0x00, data1, 0, 2);
+
+		// Convert the data to 12-bits
+		int raw_adc1 = ((data1[0] & 0x0F)* 256 + (data1[1] & 0xFF));
+		if(raw_adc1 > 2047)
+		{
+			raw_adc1 -= 4095;
+		}
+
+		// Output data to screen
+		System.out.printf("Digital value of Analog input on Channel-2: %d %n", raw_adc1);
+	}
+}
---
+++ Particle/MCP3427_2Channel.ino
@@ -0,0 +1,107 @@
+// Distributed with a free-will license.
+// Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
+// MCP3427
+// This code is designed to work with the MCP3427_I2CADC I2C Mini Module available from ControlEverything.com.
+// https://www.controleverything.com/content/Analog-Digital-Converters?sku=MCP3427_I2CADC#tabs-0-product_tabset-2
+
+#include <application.h>
+#include <spark_wiring_i2c.h>
+
+// MCP3427 I2C address is 0x68(104)
+#define Addr 0x68
+
+int raw_adc = 0;
+void setup()
+{
+  // Set variable
+  Particle.variable("i2cdevice", "MCP3427");
+  Particle.variable("rawADC", raw_adc);
+
+  // Initialise I2C communication as MASTER
+  Wire.begin();
+  // Start serial communication and set baud rate = 9600
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  unsigned int data[2];
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select configuration command
+  // Continuous conversion mode, Channel-1, 12-bit Resolution
+  Wire.write(0x10);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  delay(500);
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select data register
+  Wire.write(0x00);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Request 2 bytes of data
+  Wire.requestFrom(Addr, 2);
+
+  // Read 2 bytes of data
+  // raw_adc msb, raw_adc lsb
+  if (Wire.available() == 2)
+  {
+    data[0] = Wire.read();
+    data[1] = Wire.read();
+  }
+
+  // Convert the data
+  raw_adc = (data[0] & 0x0F) * 256 + data[1];
+  if (raw_adc > 2047)
+  {
+    raw_adc -= 4096;
+  }
+
+  // Output data to console
+  Particle.publish("Digital value on Channel-1: ", String(raw_adc));
+  delay(1000);
+
+    // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select configuration command
+  // Continuous conversion mode, Channel-2, 12-bit Resolution
+  Wire.write(0x30);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+  delay(500);
+
+  // Start I2C Transmission
+  Wire.beginTransmission(Addr);
+  // Select data register
+  Wire.write(0x00);
+  // Stop I2C Transmission
+  Wire.endTransmission();
+
+  // Request 2 bytes of data
+  Wire.requestFrom(Addr, 2);
+
+  // Read 2 bytes of data
+  // raw_adc msb, raw_adc lsb
+  if (Wire.available() == 2)
+  {
+    data[0] = Wire.read();
+    data[1] = Wire.read();
+  }
+
+  // Convert the data
+  raw_adc = (data[0] & 0x0F) * 256 + data[1];
+  if (raw_adc > 2047)
+  {
+    raw_adc -= 4096;
+  }
+
+  // Output data to console
+  Particle.publish("Digital value on Channel-2: ", String(raw_adc));
+  delay(1000);
+}
+
+
